75fcf253e59f0dc4443845c7f1ed420521a86005
Minh_Tu <minhtu777n@gmail.com>
2025-09-28 11:05:48 +0700

    adding route

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,8 +2,38 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/auth.controller');
+const CategoryController = require('../controllers/category.controller');
+const BookController = require('../controllers/book.controller');
+const BorrowController = require('../controllers/borrow.controller');
+const LogController = require('../controllers/log.controller');
+const FavouriteController = require('../controllers/favourite.controller');
 
 // Router cho dang ky, dang nhap, dang xuat
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.post('/logout', authController.logout);
+router.post('/auth/register', authController.register);
+router.post('/auth/login', authController.login);
+router.post('/auth/logout', authController.logout);
+
+// danh muc
+router.post('/categories', CategoryController.create);
+router.get('/categories', CategoryController.get);
+router.put('/categories', CategoryController.update);
+router.delete('/categories', CategoryController.delete);
+
+// sach
+router.post('/books', BookController.create);
+router.get('/books', BookController.get);
+router.put('/books', BookController.update);
+router.delete('/books', BookController.delete);
+
+// cho muon va tra sach
+router.post('/borrow', BorrowController.borrow);
+router.post('/borrow/return', BorrowController.returnBook);
+
+// Log
+router.get('/logs', LogController.getAll);
+
+// Yeu thich
+router.post('/favourites', FavouriteController.add);
+router.post('/favourites/remove', FavouriteController.delete);
+
+module.exports = router;
